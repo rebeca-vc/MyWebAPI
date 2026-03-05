0cbaa33951af7e4e1a3bdd624f38fe9c539e417c
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2026-03-05 19:17:07 -0300

    Add EF config

--- MyWebAPI/Context/AppDbContext.cs
+++ MyWebAPI/Context/AppDbContext.cs
@@ -5,8 +5,7 @@ namespace MyWebAPI.Context;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    {}
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container --> ConfigureServices()
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build(); // --> Configure
 
