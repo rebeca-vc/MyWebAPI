6cb4a5db153960a870f0ffede1cbb54a28887052
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2026-03-05 18:56:37 -0300

    Add Models

---
+++ MyWebAPI/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyWebAPI.Models;
+
+[ExcludeFromCodeCoverage]
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+
+}
+
---
+++ MyWebAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebAPI.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public float Stock { get; set; }
+    public DateTime? Created { get; set; }
+}
