1348cdd62f872f27562a0bdb1bd0d9276ea61cb4
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2026-03-05 18:56:55 -0300

    Add EF initial structure

---
+++ MyWebAPI/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Models;
+
+namespace MyWebAPI.Context;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+}
